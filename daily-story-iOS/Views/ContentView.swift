795d2119583ac7287641b90f811bc082d464caf7
山田大陽 <tai09to06y@gmail.com>
2025-06-23 18:04:59 +0900

    名前変更

---
+++ daily-story-iOS/Views/ContentView.swift
@@ -0,0 +1,105 @@
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = AuthViewModel()
+    
+    var body: some View {
+        Group {
+            if viewModel.isAuthenticated {
+                MainTabView(viewModel: viewModel)
+            } else {
+                LoginView()
+            }
+        }
+    }
+}
+
+struct MainTabView: View {
+    
+    @ObservedObject var viewModel: AuthViewModel
+    @State private var showingPostView = false
+    @State private var selection: Selection = .home
+    
+    enum Selection{
+        case home
+        case search
+        case post
+        case notification
+        case profile
+        
+    }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            //  ホームタブ
+            Text("ホーム画面")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                }
+                .tag(Selection.home)
+
+            
+            
+            //  プラン探しタブ
+            Text("プラン探し画面")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+                .tag(Selection.search)
+
+            
+            
+
+//            Button(action: {
+//                showingPostView.toggle()
+//            
+//            }) {
+//                
+//
+//            }
+//            .tabItem {
+//                Image(systemName: "plus.app")
+//            }
+            Text("投稿画面")
+                .tabItem {
+                    Image(systemName: "plus.app")
+                }
+                .tag(Selection.post)
+
+            
+            //  思い出タブ
+            Text("お知らせ画面")
+                .tabItem {
+                    Image(systemName: "speaker.square")
+                }
+                .tag(Selection.notification)
+
+            
+            //  プロフィールタブ
+            MyPageView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                }
+                .tag(Selection.profile)
+            
+        }
+        .accentColor(.pink)
+        .onChange(of: selection) { newValue in
+            if newValue == .post {
+                showingPostView = true
+                
+                selection = .home
+            }
+        }
+        
+        .fullScreenCover(isPresented: $showingPostView) {
+            PostView()
+        }
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
