afec78811b01a1bb07a5bcae51f01deb9630249d
TaiyoYamada <tai09to06y@gmail.com>
2025-08-30 15:06:52 +0900

    Revert " 地域別投稿の基盤実装（DBスキーマ・モデル・APIエンドポイント）"
    This reverts commit e25698fa7a6f62374ed34957fe9743ae28cd6391.

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\ShopController;
 use App\Http\Controllers\AdminShopController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\ImageController;
-use App\Http\Controllers\AreaController;
 
 // API動作確認用のテストルート
 Route::get('/', function () {
@@ -67,10 +66,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // 統合検索API
     Route::get('/search', [SearchController::class, 'search']);
-
-    // エリア関連API
-    Route::get('/areas', [AreaController::class, 'index']);
-    Route::get('/areas/{id}/posts', [AreaController::class, 'posts']);
 
     Route::get('/shops', [ShopController::class, 'index']);
     Route::get('/shops/search', [ShopController::class, 'search']);
